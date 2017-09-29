ed040827069eefbef20ead721ed73712ed7c64cb
Your Name <user@omids-MacBook-Pro.local>
2017-09-29 21:35:25 +0330

    my django girls app, first commit

---
+++ djangogirls/blog/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.utils import timezone
+from django.db import models
+# Create your models here.
+class Post(models.Model):
+    author= models.ForeignKey('auth.User')
+    title= models.CharField(max_length=200)
+    text= models.TextField()
+    create_date=models.DateTimeField(blank=True,null=True)
+    published_date=models.DateTimeField(blank=True,null=True)
+
+
+    def pubish(self):
+        self.published_date=timezone.now()
+        self.save()
+
+
+    def __str__(self):
+        return self.title
